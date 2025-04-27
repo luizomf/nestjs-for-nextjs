4867fea4a2bceea3286cb04f63879e2073401486
Luiz Otávio <luizomf@gmail.com>
2025-04-27 12:27:45 -0300

    Abstração para 'programar para interfaces' no NestJS com Bcrypt

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { CustomParseIntPipe } from 'src/common/pipes/custom-parse-int-pipe.pipe';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly userService: UserService,
+  ) {}
 
   @Get(':id')
   findOne(@Param('id', CustomParseIntPipe) id: number) {
-    console.log(process.env.TESTE1 || 'Padrão');
-    console.log(this.configService.getOrThrow('TESTE1'));
     return `olá do controller do user #${id}`;
+  }
+
+  @Post()
+  create(@Body() dto: CreateUserDto) {
+    return this.userService.create(dto);
   }
 }
