4ea4c5cf6cbad3524cb252483c41a3ea541e30e5
Luiz Otávio <luizomf@gmail.com>
2025-04-28 10:55:47 -0300

    Atualização de senha do usuário (currentPassword e newPassword)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -13,10 +13,10 @@ import { CustomParseIntPipe } from 'src/common/pipes/custom-parse-int-pipe.pipe'
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { Request } from 'express';
 import { AuthenticatedRequest } from 'src/auth/types/authenticated-request';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { UserResponseDto } from 'src/post/dto/user-response.dto';
+import { UserResponseDto } from './dto/user-response.dto';
+import { UpdatePasswordDto } from './dto/update-password.dto';
 
 @Controller('user')
 export class UserController {
@@ -46,6 +46,16 @@ export class UserController {
   @Patch('me')
   async update(@Req() req: AuthenticatedRequest, @Body() dto: UpdateUserDto) {
     const user = await this.userService.update(req.user.id, dto);
+    return new UserResponseDto(user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('me/password')
+  async updatePassword(
+    @Req() req: AuthenticatedRequest,
+    @Body() dto: UpdatePasswordDto,
+  ) {
+    const user = await this.userService.updatePassword(req.user.id, dto);
     return new UserResponseDto(user);
   }
 }
