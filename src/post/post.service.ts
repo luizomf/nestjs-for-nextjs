852ae35241ada46a74ec8cc01f4a9186ab2da705
Luiz Otávio <luizomf@gmail.com>
2025-04-29 12:56:24 -0300

    Criando o restante das rotas do Post + onDelete CASCADE

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -40,6 +40,18 @@ export class PostService {
     return post;
   }
 
+  async findAll(postData: Partial<Post>) {
+    const posts = await this.postRepository.find({
+      where: postData,
+      order: {
+        createdAt: 'DESC',
+      },
+      relations: ['author'],
+    });
+
+    return posts;
+  }
+
   async findOneOwnedOrFail(postData: Partial<Post>, author: User) {
     const post = await this.findOneOwned(postData, author);
 
@@ -113,5 +125,14 @@ export class PostService {
     post.published = dto.published ?? post.published;
 
     return this.postRepository.save(post);
+  }
+
+  async remove(postData: Partial<Post>, author: User) {
+    const post = await this.findOneOrFail(postData);
+    await this.postRepository.delete({
+      ...postData,
+      author: { id: author.id },
+    });
+    return post;
   }
 }
