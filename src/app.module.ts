808ebf9286acf53270c331bfe8f177be368e3e50
Luiz Otávio <luizomf@gmail.com>
2025-04-29 06:52:00 -0300

    Entity Post e entendendo synchronize e autoLoadEntities do TypeORM no NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,6 +21,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
             database: process.env.DB_DATABASE || './db.sqlite',
             synchronize: process.env.DB_SYNCHRONIZE === '1',
             autoLoadEntities: process.env.DB_AUTO_LOAD_ENTITIES === '1',
+            // entities: [User, Post],
           };
         }
 
